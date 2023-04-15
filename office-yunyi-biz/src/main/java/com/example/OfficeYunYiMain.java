80354555f175d8b01573896f81cb2b2a0cf117de
jywang <jywanglx@qq.com>
2023-04-16 01:42:39 +0800

    完善项目结构

---
+++ office-yunyi-biz/src/main/java/com/example/OfficeYunYiMain.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jywang
+ * @version 1.0
+ * @project office-yunyi
+ * @description TODO
+ * @date 2023/4/16 01:06:22
+ */
+@SpringBootApplication
+public class OfficeYunYiMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeYunYiMain.class, args);
+    }
+}
